bc7e6cf022eeca83164cff0169415598eb5170d3
wadagaku <obusumagaku.1005@gmail.com>
2015-08-03 04:03:52 +0000

    kadai

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :set_user, only: [:show, :edit, :update]
   def show 
-   @user = User.find(params[:id])
   end
   
   def new
@@ -16,12 +17,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+     if @user.update(user_params)
+          flash[:success] = "プロフィールを更新しました"
+          redirect_to @user
+      else
+          render :edit
+     end
+  end
 
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile)
   end
+  
+   def set_user
+    @user = User.find(params[:id])
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803021301) do
+ActiveRecord::Schema.define(version: 20150803031355) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150803021301) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "profile"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
